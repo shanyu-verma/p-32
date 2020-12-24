4c95d93ef59f552d8085d415fe28e7cad75fc73a
shanyu-verma <shanyuverma32@gmail.com>
2020-12-24 08:33:04 +0530

    COMMITING

--- BaseClass.js
+++ BaseClass.js
@@ -1,14 +1,15 @@
 class BaseClass{
     constructor(x, y, width, height, angle) {
         var options = {
-            'restitution':0.8,
+            'isStatic' : false,
+           
             'friction':1.0,
-            'density':1.0
+            'density':0.6
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-        this.image = loadImage("sprites/base.png");
+        //this.image = loadImage("sprites/base.png");
         World.add(world, this.body);
       }
       display(){
@@ -16,8 +17,11 @@ class BaseClass{
         push();
         translate(this.body.position.x, this.body.position.y);
         rotate(angle);
-        imageMode(CENTER);
-        image(this.image, 0, 0, this.width, this.height);
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height);
+
+        //imageMode(CENTER);
+        //image(this.image, 0, 0, this.width, this.height);
         pop();
       }
 }
--- Ground.js
+++ Ground.js
@@ -1,17 +1,36 @@
-class Ground {
-    constructor(x,y,width,height) {
+class Ground{
+
+  constructor(x,y,width,height){
       var options = {
-          isStatic: true
+          isStatic : true,
+          friction : 0.4,
+          density : 1.2
       }
+
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
-      World.add(world, this.body);
-    }
-    display(){
-      var pos =this.body.position;
+
+      
+
+      World.add(world,this.body);
+
+  }
+  
+  display(){
+      var pos = this.body.position;
+      var angle = this.body.angle; 
+      
+      push();
+      translate(pos.x,pos.y);
+      rotate(angle);
       rectMode(CENTER);
+      strokeWeight(4);
+      stroke("green");
       fill("brown");
-      rect(pos.x, pos.y, this.width, this.height);
-    }
-  };
+      rect(0,0,this.width,this.height);
+      pop();
+
+  }
+
+};
